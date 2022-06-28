a7d78c5adcfc3f4d0e0f01ea109a1fd7e29a1d00
Leonardo Ramírez <leoram72@gmail.com>
2022-06-27 19:26:20 -0500

    Desarrollo ejercicios 1 y 2 Módulos 2 Unidad 2 Diplomado

---
+++ CalculaValor.js
@@ -0,0 +1,25 @@
+let tot_mat = 0
+let desc = 0
+let tot_pago = 0
+
+const boton = document.querySelector('#calcular')
+
+boton.addEventListener('click',function(){
+    const nombre = document.querySelector('#nombre').value
+    const cantmat = document.querySelector('#cantmat').valueAsNumber
+    const valmat = document.querySelector('#valmat').valueAsNumber
+    const valpapel = 20000
+    const valcarnet = 8000
+
+    let tot_mat = (cantmat * valmat)
+    let desc = ((tot_mat * 20)/100)
+    let tot_pago = ((tot_mat - desc)+ valpapel+valcarnet)
+
+    const resp1 = document.querySelector("#totmat")
+    const resp2 = document.querySelector("#desc")
+    const resp3 = document.querySelector("#totpago")
+
+    resp1.innerHTML = `Valor total materias ${tot_mat}`
+    resp2.innerHTML = `Valor descuento ${desc}`
+    resp3.innerHTML = `Valor total a pagar ${tot_pago}`
+})
---
+++ EvaluaNumeros.js
@@ -0,0 +1,74 @@
+class EvaluaNumeros {
+    constructor(num1, num2, num3, num4){
+        this.A = num1
+        this.B = num2
+        this.C = num3
+        this.D = num4
+        this.numMay = 0
+        this.numMen = 0
+    }
+
+    verificaNumeros(num1, num2, num3, num4) {
+        let resultado = true
+        if((num1 == num2) || (num1 == num3) || (num1 == num4) || (num2 == num3) || (num2 == num4) || (num3 == num4)){
+            resultado =  false
+        }
+
+        return resultado
+    }
+
+    validaMayor(num1, num2, num3, num4){
+        if((num1 > num2) && (num1 > num3) && (num1 > num4)){
+            this.numMay = num1
+        }else{
+            if((num2 > num3) && (num2 > num4)){
+                this.numMay = num2
+            }else{
+                if(num3>num4){
+                    this.numMay = num3
+                }else{
+                    this.numMay = num4
+                }
+            }
+        }        
+    }
+
+    validaMenor(num1, num2, num3, num4){
+        if((num1 < num2) && (num1 < num3) && (num1 < num4)){
+            this.numMen = num1
+        }else{
+            if((num2 < num3) && (num2 < num4)){
+                this.numMen = num2
+            }else{
+                if(num3<num4){
+                    this.numMen = num3
+                }else{
+                    this.numMen = num4
+                }
+            }
+        }
+    }
+}
+
+const boton = document.querySelector('#ejecuta')
+
+boton.addEventListener('click',function(){
+    const ejercicioEvalua = new EvaluaNumeros(document.querySelector('#num1').valueAsNumber,
+                                                document.querySelector('#num2').valueAsNumber,
+                                                document.querySelector('#num3').valueAsNumber,
+                                                document.querySelector('#num4').valueAsNumber)
+    if(!ejercicioEvalua.verificaNumeros(ejercicioEvalua.A,ejercicioEvalua.B,ejercicioEvalua.C,ejercicioEvalua.D)) {alert('No se deben digitar numeros repetidos')}
+
+    ejercicioEvalua.validaMayor(ejercicioEvalua.A,ejercicioEvalua.B,ejercicioEvalua.C,ejercicioEvalua.D)
+    ejercicioEvalua.validaMenor(ejercicioEvalua.A,ejercicioEvalua.B,ejercicioEvalua.C,ejercicioEvalua.D)
+
+    console.log("El numero mayor: "+ejercicioEvalua.numMay)
+    console.log("El numero menor: "+ejercicioEvalua.numMen)
+
+    const resp = document.querySelector("#respuesta")
+    resp.innerHTML = `El número mayor es: ${ejercicioEvalua.numMay} y el número menor es: ${ejercicioEvalua.numMen}`
+
+    alert("Proceso Finalizado")
+})
+
+
